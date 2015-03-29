87f2f2278b5ebfc3da639a5e7d6324ca34b31559
aviralg <aviral.goel@outlook.com>
2015-03-29 17:59:05 +0530

    Basic template and color scheme in place.

---
+++ Resources.rst
@@ -0,0 +1,18 @@
+Social Icons
+============
+
+General Styling
+---------------
+http://codepen.io/ameyraut/pen/yfzog
+
+Quora Icon
+----------
+http://www.quora.com/Is-there-a-Quora-glyphicon-in-Font-Awesome
+
+
+CSS Effects
+===========
+
+1. http://codegeekz.com/css-effect-libraries-creating-beautiful-animations/
+2. http://www.hongkiat.com/blog/animatecss-css3-animation-library/
+3. https://github.com/daneden/animate.css
---
+++ junk.js
@@ -0,0 +1,93 @@
+
+.navbar-default .navbar-nav > li > a
+{
+    color: white;
+}
+
+#aviral-link:hover
+{
+    background-color: #E74C3C;
+    color: white;
+}
+
+
+#programming-link:hover
+{
+    background-color: #F7E041;
+    color: white;
+}
+
+
+#music-link:hover
+{
+    background-color: #1488C8;
+    color: white;
+
+}
+
+
+#origami-link:hover
+{
+    background-color: #30A443;
+    color: white;
+
+}
+
+#linux-link:hover
+{
+    background-color: #BFC66C;
+    color: white;
+
+}
+
+#life-link:hover
+{
+    background-color: #FF6B57;
+    color: white;
+
+}
+
+
+#fun-link:hover
+{
+    background-color: #D35400;
+    color: white;
+
+}
+
+.category-link
+{
+
+}
+
+.category-link > a
+{
+    font-weight: bold;
+    font-size: 1.2em;
+}
+
+.category-link > a:hover
+{
+    z-index:1000;
+}
+
+.category-link:hover {
+    -webkit-transform: scale(1.5) rotate(360deg);
+    -moz-transform: scale(1.5) rotate(360deg);
+    -ms-transform: scale(1.5) rotate(360deg);
+    -o-transform: scale(1.5) rotate(360deg);
+    transform: scale(1.5) rotate(360deg);
+    z-index:1000;
+    border: 10px solid black;
+}
+
+#fun-link
+{
+    background-color: white;
+}
+
+#fun-link > a
+{
+    color: #D35400;
+}
+
